33d10c4babb84b6796e89c04ad2a32598e73acf6
Sam <samuel@kopmann.com>
2019-05-23 18:39:36 +0200

    Add classes asian, black, indian, others and white to training sets

---
+++ classifier/Classifier.py
@@ -0,0 +1,73 @@
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+use_gpu = torch.cuda.is_available()
+if use_gpu:
+    print("Using CUDA")
+
+vgg16 = models.vgg16_bn()
+#vgg16.load_state_dict(torch.load("../input/vgg16bn/vgg16_bn.pth"))
+print(vgg16.classifier[6].out_features)
+
+data_dir = '../data/utk_training/'
+TRAIN = 'TRAIN'
+VAL = 'VAL'
+TEST = 'TEST'
+
+data_transforms = {
+    TRAIN: transforms.Compose([
+        # Data augmentation is a good practice for the train set
+        # Here, we randomly crop the image to 224x224 and
+        # randomly flip it horizontally.
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+    ]),
+    VAL: transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+    ]),
+    TEST: transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+    ])
+}
+
+image_datasets = {
+    x: datasets.ImageFolder(
+        os.path.join(data_dir, x),
+        transform=data_transforms[x]
+    )
+    for x in [TRAIN, VAL, TEST]
+}
+
+dataloaders = {
+    x: torch.utils.data.DataLoader(
+        image_datasets[x], batch_size=8,
+        shuffle=True, num_workers=4
+    )
+    for x in [TRAIN, VAL, TEST]
+}
+
+dataset_sizes = {x: len(image_datasets[x]) for x in [TRAIN, VAL, TEST]}
+
+for x in [TRAIN, VAL, TEST]:
+    print("Loaded {} images under {}".format(dataset_sizes[x], x))
+
+print("Classes: ")
+class_names = image_datasets[TRAIN].classes
+print(image_datasets[TRAIN].classes)
--- classifier/UTKSplit.py
+++ classifier/UTKSplit.py
@@ -16,23 +16,21 @@ for filename in os.listdir("../data/UTKFace/"):
     elif ethnicity == "3":
         folder = "indian"
 
-    copyfile("../data/UTKFace/%s" % filename, "../data/utk_split/%s/%s" % (folder, filename))
+    #copyfile("../data/UTKFace/%s" % filename, "../data/utk_split/%s/%s" % (folder, filename))
+
+    if (numProcessedImages < 20000):
+         copyfile("../data/UTKFace/%s" % filename, "../data/utk_training/TRAIN/%s/%s" % (folder, filename))
+    elif (numProcessedImages < 20500):
+         copyfile("../data/UTKFace/%s" % filename, "../data/utk_training/TEST/%s/%s" % (folder, filename))
+    else:
+        copyfile("../data/UTKFace/%s" % filename, "../data/utk_training/VAL/%s/%s" % (folder, filename))
+
     numProcessedImages = numProcessedImages + 1
 
     if (numProcessedImages % 100 == 0):
         print(numProcessedImages)
 
 
-
-numProcessedImages = 0
-for filename in os.listdir("../data/UTKFace/"):
-    if (numProcessedImages < 20000):
-        copyfile("../data/UTKFace/%s" % filename, "../data/utk_training/TRAIN/%s" % filename)
-    elif (numProcessedImages < 20500):
-        copyfile("../data/UTKFace/%s" % filename, "../data/utk_training/TEST/%s" % filename)
-    else:
-        copyfile("../data/UTKFace/%s" % filename, "../data/utk_training/VAL/%s" % filename)
-    numProcessedImages = numProcessedImages + 1
 
 
 
