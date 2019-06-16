379e85c117b4413556f2d046bdeab750f867b9b1
Kevin Birke <mail@kevinbirke.de>
2019-06-16 12:00:01 +0200

    Changed Training Command

--- cycle_gan/scripts/train_cyclegan.sh
+++ cycle_gan/scripts/train_cyclegan.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --pool_size 50 --no_dropout
+CUDA_VISIBLE_DEVICES=2 python train.py --dataroot /home/p19g2/data_race_add/train --name maps_cyclegan --model cycle_gan --pool_size 50 --no_dropout
