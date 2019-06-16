a287de82708454f647217531f32293edb8a56638
Sam <samuel@kopmann.com>
2019-06-16 12:47:31 +0200

    Remove dense layer

--- cycle_gan/models/cycle_gan_model.py
+++ cycle_gan/models/cycle_gan_model.py
@@ -169,7 +169,7 @@ class CycleGANModel(BaseModel):
         if lambda_idt > 0:
 
             vgg16 = models.vgg16_bn(pretrained=True)
-            features = list(vgg16.classifier.children())[:-3]
+            features = list(vgg16.classifier.children())[:-4]
             vgg16.classifier = nn.Sequential(*features)
 
             if torch.cuda.is_available():
