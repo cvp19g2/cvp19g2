287c829095843667d0d3b5e30774bb17d02cae31
Kevin Birke <mail@kevinbirke.de>
2019-06-27 17:48:09 +0200

    Continue Script

---
+++ cycle_gan/scripts/train_cyclegan_continue.sh
@@ -0,0 +1,2 @@
+set -ex
+CUDA_VISIBLE_DEVICES=2 python train.py --dataroot /home/p19g2/data_race_add/train --name maps_cyclegan --model cycle_gan --pool_size 50 --no_dropout --continue_train --niter 40 --niter_decay 20
