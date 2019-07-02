78c8377c539313716f4bd4263304ba84e9a38070
Sam <samuel@kopmann.com>
2019-07-02 20:05:22 +0200

    Improve resizing/cropping

--- util/ImageResizer.py
+++ util/ImageResizer.py
@@ -1,22 +1,49 @@
-import os
+import cv2
+import numpy as np
 
-from PIL import Image
-from joblib import Parallel, delayed
-import multiprocessing
+def resizeAndPad(img, size, padColor=0):
 
-#Resizing image and filling background black
-def make_square(im, min_size=224, fill_color=(0, 0, 0, 0)):
-    x, y = im.size
-    size = max(min_size, x, y)
-    new_im = Image.new('RGB', (size, size), fill_color)
-    a, b = (int((size - x) / 2), int((size - y) / 2))
-    new_im.paste(im, (a,b))
-    return new_im
+    h, w = img.shape[:2]
+    sh, sw = size
 
-def resizeImageFillBlack(filename):
-    toRezize = Image.open("../data/UTKCropped/%s"%filename).convert("RGB")
-    make_square(toRezize).save("../data/UTKResizedBlack/%s" % filename)
+    # interpolation method
+    if h > sh or w > sw: # shrinking image
+        interp = cv2.INTER_AREA
+    else: # stretching image
+        interp = cv2.INTER_CUBIC
+
+    # aspect ratio of image
+    aspect = w/h  # if on Python 2, you might need to cast as a float: float(w)/h
+
+    print(aspect)
+
+    # compute scaling and pad sizing
+    if aspect > 1: # horizontal image
+        new_w = sw
+        new_h = np.round(new_w/aspect).astype(int)
+        pad_vert = (sh-new_h)/2
+        pad_top, pad_bot = np.floor(pad_vert).astype(int), np.ceil(pad_vert).astype(int)
+        pad_left, pad_right = 0, 0
+    elif aspect < 1: # vertical image
+        new_h = sh
+        new_w = np.round(new_h*aspect).astype(int)
+        pad_horz = (sw-new_w)/2
+        pad_left, pad_right = np.floor(pad_horz).astype(int), np.ceil(pad_horz).astype(int)
+        pad_top, pad_bot = 0, 0
+    else: # square image
+        new_h, new_w = sh, sw
+        pad_left, pad_right, pad_top, pad_bot = 0, 0, 0, 0
+
+    # set pad color
+    if len(img.shape) is 3 and not isinstance(padColor, (list, tuple, np.ndarray)): # color image but only one color provided
+        padColor = [padColor]*3
+
+    # scale and pad
+    scaled_img = cv2.resize(img, (new_w, new_h), interpolation=interp)
+    scaled_img = cv2.copyMakeBorder(scaled_img, pad_top, pad_bot, pad_left, pad_right, borderType=cv2.BORDER_CONSTANT, value=padColor)
+
+    return scaled_img
 
 
-num_cores = multiprocessing.cpu_count()
-Parallel(n_jobs=num_cores)(delayed(resizeImageFillBlack)(filename) for filename in os.listdir("../data/UTKCropped/"))
+#num_cores = multiprocessing.cpu_count()
+#Parallel(n_jobs=num_cores)(delayed(resizeImageFillBlack)(filename) for filename in os.listdir("../data/UTKCropped/"))
