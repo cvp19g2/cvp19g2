4d6764ccffa58dbc1a1669ad10fb91367f23443c
Kevin Birke <mail@kevinbirke.de>
2019-07-08 20:48:07 +0200

    Change image path and name

--- cycle_gan/scripts/train_cyclegan_continue.sh
+++ cycle_gan/scripts/train_cyclegan_continue.sh
@@ -1,2 +1,2 @@
 set -ex
-CUDA_VISIBLE_DEVICES=2 python train.py --dataroot /home/p19g2/data_race_add/train --name perceptual_loss_test --model cycle_gan --pool_size 50 --no_dropout --continue_train
+CUDA_VISIBLE_DEVICES=2 python train.py --dataroot /cvhci/data/praktikumSS2019/gruppe2/CelebA/classified_aligned/train_white_black --name new_images_aligned_30 --model cycle_gan --pool_size 50 --no_dropout --continue_train
