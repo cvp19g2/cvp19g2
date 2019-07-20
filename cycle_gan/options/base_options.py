90d62bb7d699901b9d67870b812a588da3092d9e
Kevin Birke <mail@kevinbirke.de>
2019-07-20 04:20:09 +0200

    Update base_options.py

--- cycle_gan/options/base_options.py
+++ cycle_gan/options/base_options.py
@@ -23,9 +23,9 @@ class BaseOptions():
         parser.add_argument('--dataroot', required=True, help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
         parser.add_argument('--name', type=str, default='experiment_name', help='name of the experiment. It decides where to store samples and models')
         parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
-        parser.add_argument('--checkpoints_dir', type=str, default='/home/samuel/repos/cvp19g2/cycle_gan/checkpoints', help='models are saved here')
+        parser.add_argument('--checkpoints_dir', type=str, default='./cycle_gan/checkpoints', help='models are saved here')
         # model parameters
-        parser.add_argument('--model', type=str, default='test', help='chooses which model to use. [cycle_gan | pix2pix | test | colorization]')
+        parser.add_argument('--model', type=str, default='cycle_gan', help='chooses which model to use. [cycle_gan | pix2pix | test | colorization]')
         parser.add_argument('--input_nc', type=int, default=3, help='# of input image channels: 3 for RGB and 1 for grayscale')
         parser.add_argument('--output_nc', type=int, default=3, help='# of output image channels: 3 for RGB and 1 for grayscale')
         parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in the last conv layer')
